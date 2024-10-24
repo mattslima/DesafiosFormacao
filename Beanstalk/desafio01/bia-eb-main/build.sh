b0b208ee3a4b0f0a8c6e1ece6f8d8857650e9551
Matheus Lima <matheus.lima220@gmail.com>
2024-10-24 01:30:00 -0300

    teste4

--- Beanstalk/desafio01/bia-eb-main/build.sh
+++ Beanstalk/desafio01/bia-eb-main/build.sh
@@ -1,5 +1,5 @@
 versao=$(git rev-parse HEAD | cut -c 1-7)
-aws ecr get-login-password --region us-east-1 --profile bia-image | docker login --username AWS --password-stdin 593793059846.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 593793059846.dkr.ecr.us-east-1.amazonaws.com
 docker build -t bia-image .
 docker tag bia:latest 593793059846.dkr.ecr.us-east-1.amazonaws.com/bia-image:$versao
 docker push 593793059846.dkr.ecr.us-east-1.amazonaws.com/bia-image:$versao
