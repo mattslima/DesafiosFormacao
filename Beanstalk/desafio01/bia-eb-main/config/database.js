ad322ba43daa37dee2d4011ab866b5bb07f7d905
Matheus lima <matheus.lima220@gmail.com>
2024-11-24 17:23:33 -0300

    bs

--- Beanstalk/desafio01/bia-eb-main/config/database.js
+++ Beanstalk/desafio01/bia-eb-main/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   username: process.env.DB_USER || "postgres",
   password: process.env.DB_PWD || "postgres",
   database: "bia",
-  host: process.env.DB_HOST || "127.0.0.1",
-  port: process.env.DB_PORT || 5433,
+  host: process.env.DB_HOST || "bia.cbu8yu2a03r9.us-east-1.rds.amazonaws.com",
+  port: process.env.DB_PORT || 5432,
   dialect: "postgres",
 };
